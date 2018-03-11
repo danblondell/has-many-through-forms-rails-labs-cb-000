3c5c4289bcc031e4b762df25063de5349ba17eed
Dan Blondell <danblondell@gmail.com>
2018-03-11 01:33:04 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(username: "Jeb")
+Comment.all.each do |comment|
+	comment.user = User.find(1)
+end
